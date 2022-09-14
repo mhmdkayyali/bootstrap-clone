1d5f15a9f9a6e7cd97126a6a1714ed09ee894fc8
mhmdkayyali <mohammad.a.kayyali@gmail.com>
2022-09-14 12:55:20 +0300

    js

--- js/index.js
+++ js/index.js
@@ -19,3 +19,12 @@ function messagevalidation () {
     var message = document.getElementById("message")
     var inputValue = message.value
 }
+
+fetch("http://localhost/bootstrap-contactme-db/articles.php", {
+    method:'post',body: new URLSearchParams({
+        "email":email.value,
+        "full_name":full_name.value,
+        "phone_number":phone_number.value,
+        "message":message.value,
+    })})
+    .then(Response => Response.json())
