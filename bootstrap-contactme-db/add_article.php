f8d9e9a8645123000397f4028d779781fbd21ce3
mhmdkayyali <mohammad.a.kayyali@gmail.com>
2022-09-14 12:16:57 +0300

    able to add data through postman

--- bootstrap-contactme-db/add_article.php
+++ bootstrap-contactme-db/add_article.php
@@ -1,15 +1,13 @@
 <?php
 include("connection.php");
 
-$name = $_POST["email"];
+$email = $_POST["email"];
 $full_name = $_POST["full_name"];
 $phone_number = $_POST["phone_number"];
 $message = $_POST["message"];
 
-
-
-$query = $mysqli->prepare("INSERT INTO contactme(name, full_name, phone_number, message) VALUE (?, ?, ?, ?)");
-$query->bind_param("ssss", $name, $full_name, $phone_number, $message);
+$query = $mysqli->prepare("INSERT INTO contactme(email, full_name, phone_number, message) VALUE (?, ?, ?, ?)");
+$query->bind_param("ssss", $email, $full_name, $phone_number, $message);
 $query->execute();
 
 $response = [];
